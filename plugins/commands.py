f898cb40a59e317a1bd3b9bdfe74024d0f95d505
Naya Hun <shahabajtalk2@gmail.com>
2024-07-06 13:23:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -66,7 +66,7 @@ async def start(client, message):
         return
     if AUTH_CHANNEL and not await is_req_subscribed_lazy(client, message):
         try:
-            invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
+            invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL), creates_join_request=True)
         except ChatAdminRequired:
             logger.error("Hey Sona, Ek dfa check kr lo ki main Channel mei Add hu ya nhi...!")
             return
